10008f6444e72588cbcf8f04e2b69ce932090c18
Shaqlin <shaqlinmondal8905@gmail.com>
2025-07-31 00:21:51 +0530

    ..

--- connection/config.php
+++ connection/config.php
@@ -1,4 +1,5 @@
 <?php
     // config.php
-    $BASE_URL_LOCAL = "http://localhost/johar/backend/apis";
+    $BASE_URL_LOCAL = "http://localhost/johar/backend/apis"; //For Local
+    // $BASE_URL_LOCAL = "new.johartraders.in/backend/apis";  // For Live
 ?>
--- connection/db_connect.php
+++ connection/db_connect.php
@@ -1,35 +1,9 @@
 <?php
     // // Database configuration
-    // $host = 'localhost'; // Database host (usually 'localhost')
-    // $dbname = 'jt_laravel'; // Your database name
-    // $username = 'jt_laravel'; // Your database username
-    // $password = 'T4336%iuu'; // Your database password
-
-    // // DSN (Data Source Name)
-    // $dsn = "mysql:host=$host;dbname=$dbname;charset=utf8";
-
-    // try {
-    //     // Create a new PDO instance
-    //     $pdo = new PDO($dsn, $username, $password);
-
-    //     // Set the PDO error mode to exception for better error handling
-    //     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    //     // If the connection is successful, output this message
-    //     echo "Connected successfully to the database.";
-
-    // } catch (PDOException $e) {
-    //     // Catch and display any connection errors
-    //     die("Connection failed: " . $e->getMessage());
-    // }
-
-
-
-    // Database configuration
     $host = 'localhost'; // Database host (usually 'localhost')
-    $dbname = 'jt_local'; // Your database name
-    $username = 'root'; // Your database username
-    $password = ''; // Your database password
+    $dbname = 'jt_laravel'; // Your database name
+    $username = 'jt_laravel'; // Your database username
+    $password = 'T4336%iuu'; // Your database password
 
     // DSN (Data Source Name)
     $dsn = "mysql:host=$host;dbname=$dbname;charset=utf8";
@@ -42,7 +16,7 @@
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
         // If the connection is successful, output this message
-        // echo "Connected Okay. \n";
+        echo "Connected successfully to the database.";
 
     } catch (PDOException $e) {
         // Catch and display any connection errors
@@ -51,14 +25,28 @@
 
 
 
+    // Database configuration
+    // $host = 'localhost'; // Database host (usually 'localhost')
+    // $dbname = 'jt_local'; // Your database name
+    // $username = 'root'; // Your database username
+    // $password = ''; // Your database password
 
+    // // DSN (Data Source Name)
+    // $dsn = "mysql:host=$host;dbname=$dbname;charset=utf8";
 
+    // try {
+    //     // Create a new PDO instance
+    //     $pdo = new PDO($dsn, $username, $password);
 
+    //     // Set the PDO error mode to exception for better error handling
+    //     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+    //     // If the connection is successful, output this message
+    //     // echo "Connected Okay. \n";
 
-
-
-
-
+    // } catch (PDOException $e) {
+    //     // Catch and display any connection errors
+    //     die("Connection failed: " . $e->getMessage());
+    // }
 
 ?>
